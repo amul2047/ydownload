8492e75843a3f2a4ebeb3ac95f297f7c7f757fee
amul sapkota <amul.sapkota@gmail.com>
2016-09-13 23:57:51 -0400

    heroku +1

--- server.js
+++ server.js
@@ -41,8 +41,25 @@ app.post('/', function(req, res) {
 
 });
 
-app.listen(8080, function() {
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+
+app.listen(port, function() {
   console.log('Server running at http://127.0.0.1:8080/');
+});
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+  // ejs render automatically looks in the views folder
+  res.render('index');
 });
 
 
